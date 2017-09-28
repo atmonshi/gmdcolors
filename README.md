8d8616f3373bfebc7f8f5ca2fec6981c79b196bb
wa7eedem <wh7r.com@gmail.com>
2017-09-28 22:41:00 +0300

    add travis icon

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # GMDColors
+[![Build Status](https://travis-ci.org/wa7eedem/GMDColors.svg?branch=master)](https://travis-ci.org/wa7eedem/GMDColors)
+
 Google Material Design hex colors for php
+
--- src/getcolors.php
+++ src/getcolors.php
@@ -70,7 +70,7 @@ class getcolors
                     $newGoogleMaterialColors = array_keys($palette);
                 }
             }
-            
+
             $return = $newReturn;
         }
 
